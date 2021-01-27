063670ed70508cfa9c0602ef9cf6e28893708b93
Jose Ascurra <joseascurra123@gmail.com>
2021-01-27 10:41:56 -0300

    Cambios en el codigo

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -5,9 +5,9 @@ namespace Prueba\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequest;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController{
-    use DispatchesJobs,ValidatesRequest,AuthorizesRequest;
+    use DispatchesJobs,ValidatesRequests,AuthorizesRequests;
     
 }
